e9c1255341fdd139131504eed6fcf6f9ffe6c0b2
m-hailey <mele.hailey@gmail.com>
2025-04-03 13:09:41 -0600

    homework

--- Homework 10/sketch.js
+++ Homework 10/sketch.js
@@ -1,18 +1,57 @@
+var headX = 250;
+var headY = 100;
+var headDirection = 1;
+
+var eyeX = 200;
+var eyeY = 185;
+var eyeDirection = 3;
+
+var x = 100;
+var y = 200;
+var diameter = 50;
+
+var size = 25;
+var count = 0;
+var sizeDirection = 2;
+
 function setup() {
     createCanvas(400, 400);
   }
   
-  function draw() {
+  function draw() 
+{
     background(220);
-    text ('Simple Self Portrait',150,50);
-    text ('By: Lee Mele', 10,390)
-    circle (200,200,200);
-    rect (125,175,60,50);
-    rect (215,175,60,50);
-    line (185,200,215,200);
+    textSize (20)
+    text('By: Lee Mele',270,500)
+    circle (headX,headY,200);
+    headX+=headDirection;
+    if (headX >= 300 || headX <= 100)
+      {
+        headDirection *= -1;
+      }
+  
+    rect (125,eyeY,60,50);
+    eyeY += eyeDirection;
+    if (eyeY <= 0 || eyeY >= 360)
+      {
+        eyeDirection *= -1;
+      }
+  
+    rect (215,eyeY,60,50);
+    line (185,eyeY,215,eyeY);
     strokeWeight(10);
-    point (200,240);
-    triangle (125,400,200,305,275,400);
-    line (155,350,100,300);
-    line (240,350,290,300)
-  }
+    point (headX,headX);
+   triangle (headX,400,headX,305,headY,400);
+    line (155,eyeY,100,300);
+    line (240,eyeY,290,300);
+  
+  textSize(size);
+    size+= sizeDirection;
+    count++;
+    if(count > 5)
+    {
+        sizeDirection *=-1;
+        count = 0;
+    }
+  text('Simple Self Portrait',100,50);
+}
